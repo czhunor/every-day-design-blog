6c659e220b6cd138db1d3edf9fc9226758ad905f
Hunor Czaka <czhunor@gmail.com>
2021-04-26 22:22:10 +0200

    .

---
+++ content/posts/my-new-blog-post.md
@@ -0,0 +1,8 @@
+---
+title: "My new blog post"
+date: "2021-04-18"
+---
+
+Do Pandas eat bananas? Check out this short video that shows that yes! pandas do seem to really enjoy bananas!
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4SZl1r2O_bY" frameborder="0" allowfullscreen></iframe>
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,31 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
